ccac0994d436b575e8d31d530a57a42d6a27df3c
Meghan Sun <meghansun322@gmail.com>
2022-01-25 21:51:31 -0500

    wrapper fs.readFile() around server.listen

--- server.js
+++ server.js
@@ -9,36 +9,34 @@ const args = require("minimist")(process.argv.slice(2));
 args["port"];
 // Define a const `port` using the argument from the command line.
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = process.env.port || 3000;
+const port = args.port || process.env.PORT || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module.
 // The function must read a file located at `./www/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-
-// If there is an error, put it on the console error, return, and exit with error code 1.
-// Do not be nice about exiting.
-
-// Define a const `server` as an arrow function using http.createServer.
-// Use the documentation for the node.js http module.
-// The function should have three responses:
-// 1. status code 200,
-// 2. set a header with content type `text/html`, and
-// 3. end with the data that you are reading in from ./www/index.html.
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  // This is an async call
-  fs.readFile("./www/index.html", "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-      return process.exit(1);
-    }
-    res.end(data); // data has the contents of index.html
+fs.readFile("./www/index.html", "utf8", (err, data) => {
+  // If there is an error, put it on the console error, return, and exit with error code 1.
+  // Do not be nice about exiting.
+  if (err) {
+    console.error(err);
+    return;
+    process.exit(1);
+  }
+  // Define a const `server` as an arrow function using http.createServer.
+  // Use the documentation for the node.js http module.
+  // The function should have three responses:
+  // 1. status code 200,
+  // 2. set a header with content type `text/html`, and
+  // 3. end with the data that you are reading in from ./www/index.html.
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end(data);
   });
+  // Start the `server` const listening on the port defined by argument in your `port` const.
+  // Put the exact message `Server listening on port ${port}` on the console log.
+  server.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+  });
+  // That's it! You're all done!
 });
-// Start the `server` const listening on the port defined by argument in your `port` const.
-// Put the exact message `Server listening on port ${port}` on the console log.
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`);
-});
-// That's it! You're all done!
